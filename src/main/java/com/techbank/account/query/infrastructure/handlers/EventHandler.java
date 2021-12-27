a529ee4e27034253e7e4b403206f34ff320a86e1
PavelBlinets <pavelblinets@gmail.com>
2021-12-27 22:07:56 +0300

    event handling

---
+++ src/main/java/com/techbank/account/query/infrastructure/handlers/EventHandler.java
@@ -0,0 +1,16 @@
+package com.techbank.account.query.infrastructure.handlers;
+
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.AccountsClosedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawnEvent;
+
+public interface EventHandler {
+    void on(AccountOpenedEvent event);
+
+    void on(FundsDepositedEvent event);
+
+    void on(FundsWithdrawnEvent event);
+
+    void on(AccountsClosedEvent event);
+}
